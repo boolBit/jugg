62c393e1b48027b9d583650e59168f419b0f60c5
ning.li <ning.li@duitang.com>
2016-02-15 15:53:01 +0800

    feature:init

---
+++ src/main/java/lorabit/security/UserInfo.java
@@ -0,0 +1,53 @@
+package lorabit.security;
+
+/**
+ * @author lorabit
+ * @since 16-2-15
+ */
+public class UserInfo {
+    private long userId;
+    private String nickname;
+    private String avatar;
+
+    public UserInfo(long userId, String nickname, String avatar) {
+        this.userId = userId;
+        this.nickname = nickname;
+        this.avatar = avatar;
+    }
+
+    public UserInfo(long userId, String nickname) {
+        this.userId = userId;
+        this.nickname = nickname;
+        this.avatar = "";
+    }
+
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public static UserInfo create(long id, String name) {
+        UserInfo info = new UserInfo(id, name);
+        return info;
+    }
+}
---
+++ src/test/java/test.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+/**
+ * @author lorabit
+ * @since 16-2-14
+ */
+public class test {
+    @Test
+    public void yu(){
+        System.out.println(20 %11);
+        System.out.println(20 %13);
+        System.out.println(20 %24);
+    }
+}
